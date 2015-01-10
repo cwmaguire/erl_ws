37e9cd7edf764f51067cb849efa49cc8aa06ab5a
Chris Maguire <cwmaguire@gmail.com>
2015-01-10 15:02:44 -0800

    Fixed form POST (needed name attribute)
    In order for the HTML form to send the text field the input element has
    to have a name field.
    
    Reference: http://www.w3schools.com/html/html_forms.asp

--- src/form_handler.erl
+++ src/form_handler.erl
@@ -21,17 +21,43 @@ handle(Req, State=#state{}) ->
 
     {Host, Req3} = cowboy_req:host(Req2),
     {HostURL, Req4} = cowboy_req:host_url(Req3),
+    {ok, Body, Req5} = cowboy_req:body(Req4),
+    {ok, QsKVs, Req6} = cowboy_req:body_qs(Req5),
+    io:format("Query string KV pairs:~n\t~p~n", [QsKVs]),
 
-    {ok, Req5} = cowboy_req:reply(200,
+    {Path, Req7} = cowboy_req:path(Req6),
+    {Peer, Req8} = cowboy_req:peer(Req7),
+    {Port, Req9} = cowboy_req:port(Req8),
+    io:format("Path: ~p~n", [Path]),
+    io:format("Peer: ~p~n", [Peer]),
+    io:format("Port: ~p~n", [Port]),
+
+
+    {ok, Req10} = cowboy_req:reply(200,
         [{<<"content-type">>, <<"text/html">>}],
         ["<html><body>Has body: ",
          atom_to_list(HasBody), "<br>",
          BinHeaders, "<br>",
          "Host: ", Host, "<br>",
          "Host URL: ", HostURL, "<br>",
+         "Body: ", Body, "<br>",
+         "Path: ", Path, "<br>",
+         "Peer: ", peer_to_list(Peer), "<br>",
+         "Port: ", integer_to_list(Port), "<br>",
          "</body></html>"],
-        Req4),
-    {ok, Req5, State}.
+        Req9),
+    {ok, Req10, State}.
 
 terminate(_Reason, _Req, _State) ->
 	ok.
+
+peer_to_list({{IP1,IP2,IP3,IP4}, PeerPort}) ->
+    [integer_to_list(IP1),
+     ".",
+     integer_to_list(IP2),
+     ".",
+     integer_to_list(IP3),
+     ".",
+     integer_to_list(IP4),
+     ":",
+     integer_to_list(PeerPort)].
